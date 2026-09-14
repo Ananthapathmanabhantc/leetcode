e3ea478ff8e871fd5befb4ba641fd2db7e713804
Ananthapathmanabhan T C <ananthapathmanabhantc@gmail.com>
2026-09-14 20:53:11 +0530

    File updated at 14/9/2026, 8:53:07 pm

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,23 +1,17 @@
-// Last updated: 14/9/2026, 8:51:49 pm
+// Last updated: 14/9/2026, 8:53:07 pm
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int ls = s.length();
-        int lt = t.length();
-        if (ls != lt)
-            return false;
-        String[] cs = new String[ls];
-        String[] ct = new String[lt];
-        for (int i = 0; i < ls; i++) {
-            cs[i] = s.substring(i, i + 1);
-            ct[i] = t.substring(i, i + 1);
-        }
-        Arrays.sort(cs);
-        Arrays.sort(ct);
-        for (int i = 0; i < ls; i++) {
-            if (cs[i].compareTo(ct[i])!=0)
-                return false;
-        }
+        if(s.length() != t.length())
+           return false;
 
-        return true;
+
+           char[] a = s.toCharArray();
+           char[] b = t.toCharArray();
+
+           Arrays.sort(a);
+           Arrays.sort(b);
+
+        return Arrays.equals(a,b);
+
     }
 }
